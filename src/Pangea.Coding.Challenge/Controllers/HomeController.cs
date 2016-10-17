f1634ca59fcdbc851279693830acec83ee53a8dc
Ryan McCullagh <me@ryanmccullagh.com>
2016-10-16 21:14:12 -0500

    remove obsolete actions

--- src/Pangea.Coding.Challenge/Controllers/HomeController.cs
+++ src/Pangea.Coding.Challenge/Controllers/HomeController.cs
@@ -28,24 +28,5 @@ namespace Pangea.Coding.Challenge.Controllers
 
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
     }
 }
